0afb04c2a09589e93f9c69ff446d2fc415100a24
Omar El Sahlah <98979833+FullStackin@users.noreply.github.com>
2023-05-13 12:37:18 -0700

    docs: Add User and Authentication

---
+++ server/controllers/auth.js
@@ -0,0 +1,27 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+// REGISTER USER
+export const register = async (req, res) => {
+     try {
+        const {
+            firstName,
+            lastName,
+            email,
+            password,
+            picturePath,
+            friends,
+            location,
+            occupation
+        } = req.body;
+        const salt = await bcrypt.genSalt();
+        const passwordHash = await bcrypt.hash(password, salt);
+
+        const newUser = new User({
+            
+        })
+     } catch (err){
+
+     }
+}
--- server/index.js
+++ server/index.js
@@ -8,6 +8,8 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import { register } from "./controllers/auth.js"
+
 
 // CONFIGURATION:
 const __filename = fileURLToPath(import.meta.url);
@@ -32,8 +34,11 @@ const storage = multer.diskStorage({
     cb(null, file.originalname);
   },
 });
-
 const upload = multer({ storage });
+
+// ROUTES + FILES
+app.post("/auth/register", upload.single("picture"), register); // Middleware
+
 
 // MONGOOSE SETUP:
 const PORT = process.env.PORT || 6001; // back up port incase original doesn't work.
