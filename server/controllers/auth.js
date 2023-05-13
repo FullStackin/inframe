69a7fef987b919a5a088499c3207a69f55ffeb3b
Omar El Sahlah <98979833+FullStackin@users.noreply.github.com>
2023-05-13 12:48:49 -0700

    docs: Add completed auth code

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -18,6 +18,21 @@ export const register = async (req, res) => {
     const salt = await bcrypt.genSalt();
     const passwordHash = await bcrypt.hash(password, salt);
 
-    const newUser = new User({});
-  } catch (err) {}
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password: passwordHash,
+      picturePath,
+      friends,
+      location,
+      occupation,
+      viewedProfile: Math.floor(Math.random() * 10000),
+      impressions: Math.floor(Math.random() * 10000),
+    });
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser); // send user correct status
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
